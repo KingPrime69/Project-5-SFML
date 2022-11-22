81cd505c1811c35cab2419ed620e6b2ac6bd5fab
KingPrime69 <quentin.ripot@free.fr>
2022-11-22 16:46:58 +0100

    start sfml

--- Source.cpp
+++ Source.cpp
@@ -1,39 +1,76 @@
-# include "Caesar.h "
-# include <string>
-# include <iostream>
+#include <iostream>
 
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
 
-
-static void encryptString(Caesar& encryptionMethod, std::string const& toEncrypt)
-{
-	size_t len = toEncrypt.size();
-	encryptionMethod.reset();
-	for (size_t i = 0; i < len; i++)
-	{
-		encryptionMethod.encryptChar(toEncrypt[i]);
-	}
-	std::cout << std::endl;
-}
-static void decryptString(Caesar& encryptionMethod,
-	std::string const& toDecrypt)
-{
-	size_t len = toDecrypt.size();
-	encryptionMethod.reset();
-	for (size_t i = 0; i < len; i++)
-	{
-		encryptionMethod.decryptChar(toDecrypt[i]);
-	}
-	std::cout << std::endl;
-}
 
 int main()
 {
-	Caesar c;
-	encryptString(c, "Je clair Luc, ne pas ? Ze woudrai un kekos !");
-	decryptString(c, "Mi isirb Xhq, ew jvo ? Zf zszjyir fz ytafk !"
-	);
-	encryptString(c, "KIKOO ");
-	encryptString(c, "LULZ XD");
-	decryptString(c, "Ziqivun ea Ndcsg.Wji !");
-	return 0;
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(1200, 800), "SFML window");
+    window.setFramerateLimit(60);
+
+    // Load a sprite to display
+    sf::Texture texture;
+    if (!texture.loadFromFile("sprite/spriteSheet.png"))return EXIT_FAILURE;
+    sf::Sprite laporeille(texture);
+    laporeille.setTexture(texture);
+    laporeille.setTextureRect(sf::IntRect(81, 25, 25, 27));
+    laporeille.scale(5, 5);
+    laporeille.setPosition(200, 300);
+
+    sf::Sprite salameche(texture);
+    salameche.setTexture(texture);
+    salameche.setTextureRect(sf::IntRect(1044, 338, 11, 18));
+    salameche.scale(5, 5);
+    salameche.setPosition(500, 500);
+
+
+
+    // Create a graphical text to display
+    sf::Font font2;
+    if (!font2.loadFromFile("pokemonSolid.ttf"))return EXIT_FAILURE;
+    sf::Text text2("Pokémon", font2, 100);
+    text2.setFillColor(sf::Color::Yellow);
+
+    sf::Font font;
+    if (!font.loadFromFile("pokemonHollow.ttf"))return EXIT_FAILURE;
+    sf::Text text("Pokémon", font, 100);
+    text.setFillColor(sf::Color::Blue);
+
+
+    int count = 0;
+    int frame = 0;
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        // Clear screen
+        window.clear();
+        // Draw the sprite
+        count++;
+        if (count == 15) {
+            frame++;
+            laporeille.setTextureRect(sf::IntRect(81+27*frame, 25, 25, 27));
+            salameche.setTextureRect(sf::IntRect(1044 + 18 * frame, 338, 11, 18));
+            count = 0;
+            if (frame == 3)frame = 0;
+        }
+        window.draw(laporeille);
+        window.draw(salameche);
+
+        // Draw the string
+        window.draw(text2);
+        window.draw(text);
+        // Update the window
+        window.display();
+    }
+    return EXIT_SUCCESS;
 }
